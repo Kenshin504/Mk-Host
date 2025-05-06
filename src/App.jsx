aef5d4b8555d1122322104bd8b4f6bfeba3f229e
Ryan <ryanlumbis712@gmail.com>
2025-05-06 08:02:59 +0800

    Version 1.6.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { useState } from "react";
 import { UserProvider } from "./components/UserContext"; // Import the UserProvider
 import Login from "./pages/Login";
